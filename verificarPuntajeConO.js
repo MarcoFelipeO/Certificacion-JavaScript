f52c090ef466d511717b1d0929cb2aac6154dce5
Marco Orellana <mafeorellana09@gmail.com>
2024-08-09 01:21:57 -0400

    ejercicio con %restos

---
+++ verificarPuntajeConO.js
@@ -0,0 +1,8 @@
+/* Escribe tu código aquí */
+function verificarPuntaje(puntaje){
+    if(puntaje >90 || puntaje <10){
+        return "Califica para bono"
+    }
+    return "No califica para bono";
+}
+/* Fin */
