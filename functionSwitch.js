a75f9edefe5159680ae8f5432086c994bdbf09fe
Marco Orellana <mafeorellana09@gmail.com>
2024-08-11 22:53:24 -0400

    Proyecto de ExpressJS y AngularJS

---
+++ functionSwitch.js
@@ -0,0 +1,17 @@
+/* Escribe tu código aquí */
+function atencionAlCliente(numero){
+    switch(numero){
+        case 1:
+            return "Contactar a un vendedor";
+        break;
+        case 2:
+            return "Esperar a ser atendido";
+        break;
+        case 3:
+            return "Contactar a soporte";
+        break;
+        default:
+            return "Opción inválida";
+    }
+}
+/* Fin */
