734819b00f702a0b4ab62798bd3ba3d7c46f322f
Marco Orellana <mafeorellana09@gmail.com>
2024-08-14 02:09:53 -0400

    function de concatear y usar el metodo toUpperCase()

---
+++ aMayusculas.js
@@ -0,0 +1,10 @@
+ /* Escribe tu código aquí */
+function aMayusculas(texto1, texto2) {
+    //junto ambos textos
+   const juntar = texto1+ texto2;
+   // aqui debo llamar a la constante y aplicar el metodo
+   mayusculaJuntar = juntar.toUpperCase();
+   return mayusculaJuntar;
+}
+
+/* Fin */
