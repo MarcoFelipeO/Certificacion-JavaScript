80c2ea10c16d8dfe747b3605a059edfdf38742fa
Marco Orellana <mafeorellana09@gmail.com>
2024-08-15 12:01:50 -0400

    function aMinusculasPrimero

---
+++ aMinusculasPrimero.js
@@ -0,0 +1,12 @@
+/* Escribe tu código aquí */
+function aMinusculasPrimero(texto1,texto2){
+
+   const minuscula = texto1.toLowerCase();
+   const concatenar = minuscula + texto2;
+   
+    return concatenar;
+}
+ console.log(aMinusculasPrimero("HOLA " , "Mundo"));
+
+
+/* Fin */
