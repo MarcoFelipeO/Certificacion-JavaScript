d1cce5e33b6b4ed224a969e7ae28821c9895ee5e
Marco Orellana <mafeorellana09@gmail.com>
2024-08-15 17:20:00 -0400

    function con length

---
+++ largoTexto.js
@@ -0,0 +1,11 @@
+/* Escribe tu código aquí */
+function largoTexto(texto,texto2){
+    const largo = texto.length;
+    const largo2 = texto2.length;
+     const concatenarLargo = largo+largo2;
+        return concatenarLargo;
+}
+
+console.log(largoTexto(" AAAAAAAAAaaaaaaaaaaaaaaaa,", "UUUUUUUUUUUUUaaaaaaaaaaaaaaaaaaaaaahhhhhhhhh"));
+
+/* Fin */
