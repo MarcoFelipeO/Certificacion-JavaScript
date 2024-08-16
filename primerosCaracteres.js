6c68bd54a26b14c966d1707d2fcaba1d54d9d08f
Marco Orellana <mafeorellana09@gmail.com>
2024-08-15 23:55:26 -0400

    function con substring(0,8)

---
+++ primerosCaracteres.js
@@ -0,0 +1,9 @@
+/* Escribe tu código aquí */
+function primerosCaracteres(texto){
+    const comienza = texto.substring(0,8);
+    return comienza;
+}
+
+console.log(primerosCaracteres("Hola como estamos el dia de hoy?"));
+
+/* Fin */
