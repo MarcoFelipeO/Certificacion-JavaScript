4651d91a5e49419a39776623e4e4cadee49f0718
Marco Orellana <mafeorellana09@gmail.com>
2024-08-17 04:35:59 -0400

    agregando function con dos parametros en substring

---
+++ exceptoLosPrimeros.js
@@ -0,0 +1,6 @@
+/* Escribe tu código aquí */
+function exceptoLosPrimeros(texto,cantidad){
+   return texto.substring(cantidad);
+}
+/* Fin */
+console.log(exceptoLosPrimeros("Hola mundo como te ha ido",6));
