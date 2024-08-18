8aff3ba3cb01d603a8dcc58ad0cff590ded7c859
Marco Orellana <mafeorellana09@gmail.com>
2024-08-18 15:46:58 -0400

    function con logicas

---
+++ transformacionCondicional.js
@@ -0,0 +1,11 @@
+/* Escribe tu código aquí */
+function transformacionCondicional(frase){
+    if(frase.charAt(0) === "a"  ){
+        return frase.toUpperCase();
+    }
+    return frase.toLowerCase();
+}
+
+
+console.log(transformacionCondicional("ahora vamos a ver"));
+/* Fin */
