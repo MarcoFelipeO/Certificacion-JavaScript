c8678fba5ccc5442ac68f268bfa166eab6b8d780
Marco Orellana <mafeorellana09@gmail.com>
2024-08-18 00:07:28 -0400

    function que busca el largo con length, se aplica length a el parametro de la function y este devuelve al cantidad de la cadena, si esta es mayor a 10 etonces dice mucho texto, sino retornar un poco texto ;)

---
+++ textoLargoCorto.js
@@ -0,0 +1,12 @@
+/* Escribe tu código aquí */
+function textoLargoCorto(texto){
+    const textoTotal = texto.length;
+    if(textoTotal >10){
+        return "Mucho texto";
+    }
+    return "Poco texto";
+}
+
+
+
+/* Fin */
