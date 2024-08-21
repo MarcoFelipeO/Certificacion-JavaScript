8b16626bd25d3e177eebcd1a2616fefed4c36049
Marco Orellana <mafeorellana09@gmail.com>
2024-08-21 18:16:48 -0400

    function con logicas

---
+++ modificarPorElMayor.js
@@ -0,0 +1,13 @@
+/* Escribe tu código aquí */
+function modificarPorElMayor(arreglo,numero1,numero2){
+    
+    const numeroMayor = Math.max(numero1,numero2);
+    
+    arreglo[0] = numeroMayor;
+
+    return arreglo; 
+}
+
+
+
+/* Fin */
