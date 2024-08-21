05651445e5733604340ce0dc40dc40dc959c6a20
Marco Orellana <mafeorellana09@gmail.com>
2024-08-21 18:33:04 -0400

    function con logicas

---
+++ borrarAlFinal.js
@@ -0,0 +1,8 @@
+/* Escribe tu código aquí */
+function borrarAlFinal(arreglo){
+    //accedemos al ultimo arreglo y eliminamos 
+     arreglo.splice(arreglo.length -1,1); 
+
+    return arreglo;
+}
+/* Fin */
