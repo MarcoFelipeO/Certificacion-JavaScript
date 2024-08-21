5573f7f4fcf488f80b371650521c05c8d733e3ef
Marco Orellana <mafeorellana09@gmail.com>
2024-08-20 22:09:21 -0400

    function con logicas

---
+++ ultimoYPrimero.js
@@ -0,0 +1,13 @@
+/* Escribe tu código aquí */
+function ultimoYPrimero(arreglo){
+    const primero = arreglo[0];
+    const ultimo = arreglo[arreglo.length -1];
+
+    const concatenar = ultimo+primero;
+    
+    return concatenar;
+}
+
+
+
+/* Fin */
