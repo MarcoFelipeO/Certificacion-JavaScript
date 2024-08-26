0478d176adb167496085ae5c543c629265aec9fd
Marco Orellana <mafeorellana09@gmail.com>
2024-08-25 23:56:36 -0400

    function con logicas

---
+++ contarDeDosEnDos.js
@@ -0,0 +1,13 @@
+/* Escribe tu código aquí */
+function contarDeDosEnDos(numero){
+    i = 2;
+    while(i <= numero){
+        console.log(i);
+        i+=2;
+    }
+}
+/* Fin */
+
+contarDeDosEnDos(10)
+contarDeDosEnDos(6)
+contarDeDosEnDos(14)
