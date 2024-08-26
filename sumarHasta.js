d2f422ed0a212c304fae55edee4ccdda94b36ac0
Marco Orellana <mafeorellana09@gmail.com>
2024-08-26 02:19:26 -0400

    function con logicas

---
+++ sumarHasta.js
@@ -0,0 +1,14 @@
+/* Escribe tu código aquí */
+function sumarHasta(numero){
+    i = 1;
+    suma = 0;
+    while(i <= numero){
+       suma +=i;
+       i++;
+    }
+    return suma;
+}
+
+
+
+/* Fin */
