5c0c60e714451c00ab0bd985659fc2d4f2b6d012
Marco Orellana <mafeorellana09@gmail.com>
2024-08-28 03:33:18 -0400

    function con logicas

---
+++ productoria.js
@@ -0,0 +1,15 @@
+/* Escribe tu código aquí */
+function productoria (numero){
+     let resultado = 1;
+     let contador = 1;
+
+    while(contador <= numero){
+        resultado *= contador; 
+        contador++;
+    }
+    return resultado;
+}
+
+
+
+/* Fin */
