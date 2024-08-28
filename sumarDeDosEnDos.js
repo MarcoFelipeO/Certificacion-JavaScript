53fb6c535fbb3aba238eb2c48871a5f116b4c1a5
Marco Orellana <mafeorellana09@gmail.com>
2024-08-28 02:13:21 -0400

    function con logicas

---
+++ sumarDeDosEnDos.js
@@ -0,0 +1,13 @@
+/* Escribe tu código aquí */
+function sumarDeDosEnDos(desde,hasta){
+   let suma = 0;
+    while(desde <= hasta){
+        suma += desde; 
+        desde += 2;
+    }
+    return suma;
+}
+
+
+
+/* Fin */
