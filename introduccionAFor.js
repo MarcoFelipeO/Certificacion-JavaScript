71e58d352fedb58719125f9de7089522af31da84
Marco Orellana <mafeorellana09@gmail.com>
2024-08-28 20:18:00 -0400

    function con logicas

---
+++ introduccionAFor.js
@@ -0,0 +1,9 @@
+/* Escribe tu código aquí */
+function introduccionAFor(){
+    for(contador=1; contador <=15; contador++){
+        console.log(contador);
+    }
+}
+/* Fin */
+
+introduccionAFor()
