33f5cab60dabc5896132a0dc2181c2f1b66dccdb
Marco Orellana <mafeorellana09@gmail.com>
2024-08-28 23:00:21 -0400

    function con logicas

---
+++ mostrarNumeros.js
@@ -0,0 +1,12 @@
+/* Escribe tu código aquí */
+function mostrarNumeros(inicio, fin){
+    for(inicio; inicio <= fin; inicio++){
+        console.log(inicio);
+    }
+}
+
+
+
+/* Fin */
+mostrarNumeros(4,9)
+mostrarNumeros(20,25)
