e218a57425229f9de0389d9c625b48bb1f63835c
Marco Orellana <mafeorellana09@gmail.com>
2024-08-31 12:10:53 -0400

    repaso de funcion ademas de comprender un for con un if dentro con resto 0 para poder contar numeros que solo sean pares ;)

--- sumatoriaPares.js
+++ sumatoriaPares.js
@@ -1,15 +1,11 @@
 /* Escribe tu código aquí */
-
 function sumatoriaPares(numero){
-
     suma = 0;
     for(contador = 1; contador <= numero; contador++){
         if(contador % 2 === 0){
-            suma += contador; 
+            suma += contador;
         }
     }
     return suma;
 }
-
-
 /* Fin */
