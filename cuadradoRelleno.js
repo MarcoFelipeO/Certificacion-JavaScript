fc7cb76b892dd54f4358fa0e937aeae736428173
Marco Orellana <mafeorellana09@gmail.com>
2024-09-01 17:39:16 -0400

    function con for

---
+++ cuadradoRelleno.js
@@ -0,0 +1,12 @@
+/* Escribe tu código aquí */
+function cuadradoRelleno(n){
+    const fila= '*'.repeat(n);
+    for(i = 0; i < n; i++){
+        console.log(fila);
+    }
+    }
+/* Fin */
+
+cuadradoRelleno(4)
+cuadradoRelleno(2)
+cuadradoRelleno(6)
